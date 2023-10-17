ba5d33022e789969da2c8e4241fe5812acc41770
Amr Mousa <amrmousa1682@gmail.com>
2023-10-18 02:37:32 +0300

    change project structure

--- Send Email/src/main/java/com/example/mailsend/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.example.mailsend.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-
-public interface EmailService  {
-    String sendMail(MultipartFile[] file, String to,String[] cc, String subject, String body);
-}
